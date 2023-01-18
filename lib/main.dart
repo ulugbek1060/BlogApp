22cc0520131f6448671bbbab5e9a287205611803
ulugbek1060 <ulugg1060@gmail.com>
2023-01-18 09:10:10 +0500

    changed keys

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
